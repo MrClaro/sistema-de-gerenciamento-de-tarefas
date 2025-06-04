23815ddb0af9ace77c2269bb2aa26b496a22c352
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-04 00:13:10 -0300

    feat(controller): adicionado validação de usuário logado a partir de um guard jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,31 @@
-import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Get,
+	Param,
+	Patch,
+	Post,
+	UseGuards,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UserService } from "./user.service";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller("user")
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Get()
+	@UseGuards(JwtAuthGuard)
 	async findAll() {
 		return await this.userService.findAll();
 	}
 
 	@Get(":id")
+	@UseGuards(JwtAuthGuard)
 	async findById(@Param("id") userId: string) {
 		return await this.userService.findById(userId);
-	}
-
-	@Get()
-	async findByEmail(@Body("email") email: string) {
-		return await this.userService.findByEmail(email);
 	}
 
 	@Post()
@@ -28,6 +34,7 @@ export class UserController {
 	}
 
 	@Patch(":id")
+	@UseGuards(JwtAuthGuard)
 	async update(@Param("id") userId: string, @Body() data: UpdateUserDto) {
 		return await this.userService.update(userId, data);
 	}
