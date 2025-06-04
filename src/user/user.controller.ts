ccef8cb28978ebf5707832eb308152aa44196ee0
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-04 03:43:37 -0300

    feat(controller): adicionado annottations para configuração do swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,17 +13,21 @@ import { UpdateUserDto } from "./dto/update-user.dto";
 import { UserService } from "./user.service";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { Roles } from "src/auth/decorator/roles.decorator";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
-@Controller("user")
+@ApiTags("Users")
+@Controller("users")
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
+	@ApiBearerAuth("JWT-auth")
 	@Get()
 	@UseGuards(JwtAuthGuard)
 	async findAll() {
 		return await this.userService.findAll();
 	}
 
+	@ApiBearerAuth("JWT-auth")
 	@Get(":id")
 	@UseGuards(JwtAuthGuard)
 	async findById(@Param("id") userId: string) {
@@ -35,12 +39,14 @@ export class UserController {
 		return await this.userService.create(data);
 	}
 
+	@ApiBearerAuth("JWT-auth")
 	@Patch(":id")
 	@UseGuards(JwtAuthGuard)
 	async update(@Param("id") userId: string, @Body() data: UpdateUserDto) {
 		return await this.userService.update(userId, data);
 	}
 
+	@ApiBearerAuth("JWT-auth")
 	@Delete(":id")
 	@Roles("ADMIN")
 	@UseGuards(JwtAuthGuard)
