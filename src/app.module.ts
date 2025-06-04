933fcdcb986fd8bd22ae9ba4c88b00cabbd05455
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-03 23:22:41 -0300

    feat(app): adicionado modulo de autentificação ao app e configuração para ser utilizado globalmente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
 import { TaskModule } from "./task/task.module";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-	imports: [UserModule, TaskModule],
+	imports: [
+		UserModule,
+		TaskModule,
+		AuthModule,
+		ConfigModule.forRoot({ isGlobal: true }),
+	],
 	controllers: [],
 	providers: [],
 })
