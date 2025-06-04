9bd890c3871221c54f621589a3e468b847c7c304
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-03 21:28:15 -0300

    feat(dto): criado DTO para atualização das tasks

---
+++ src/task/dto/update-task.dto.ts
@@ -0,0 +1,32 @@
+import {
+	IsBoolean,
+	IsDate,
+	IsEnum,
+	IsOptional,
+	IsString,
+} from "class-validator";
+import { TaskStatusEnum } from "../enums/task-status.enum";
+
+export class UpdateTaskDto {
+	@IsOptional()
+	@IsString({ message: "Title must be a string" })
+	title?: string;
+
+	@IsOptional()
+	@IsString({ message: "Description must be a string" })
+	description?: string;
+
+	@IsOptional()
+	@IsEnum(TaskStatusEnum, {
+		message: `Status must be one of the following values: ${Object.values(TaskStatusEnum).join(", ")}`,
+	})
+	status?: TaskStatusEnum;
+
+	@IsOptional()
+	@IsDate({ message: "DueDate must be a valid date" })
+	dueDate?: Date;
+
+	@IsOptional()
+	@IsBoolean({ message: "IsActive must be a boolean" })
+	isActive?: boolean;
+}
