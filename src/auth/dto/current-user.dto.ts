04fb2cd14ce1b9689a9febe4f918f4a96b8da17e
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-04 01:33:11 -0300

    feat(dto): adicionado role ao current-user dto

--- src/auth/dto/current-user.dto.ts
+++ src/auth/dto/current-user.dto.ts
@@ -1,6 +1,9 @@
+import { Role } from "src/user/enum/user-role.enum";
+
 export class CurrentUserDto {
 	userId: string;
 	name: string;
+	roles: Role[];
 
 	constructor(partial: Partial<CurrentUserDto>) {
 		Object.assign(this, partial);
