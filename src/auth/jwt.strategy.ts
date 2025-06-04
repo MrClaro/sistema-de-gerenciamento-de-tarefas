a42733e43b931540b92015b4d2d0106f6e38ae5e
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-04 00:15:13 -0300

    feat(strategy): criado strategy para lidar com os tokens jwt

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+
+export interface JwtPayload {
+	sub: string;
+	name: string;
+	iat?: number;
+	exp?: number;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(config: ConfigService) {
+		super({
+			secretOrKey: config.getOrThrow("JWT_SECRET"),
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+		});
+	}
+
+	async validate(payload: JwtPayload): Promise<any> {
+		return { userId: payload.sub, name: payload.name };
+	}
+}
