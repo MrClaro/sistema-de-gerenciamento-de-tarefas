b48b84f0d3e509d4537a58ca6dfc72effdf4714b
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-04 00:14:11 -0300

    feat(service): adicionado obtenção do usuário atualmente logado

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -9,31 +9,43 @@ export class TaskService {
 	constructor(private prisma: PrismaService) {}
 
 	// Retrieves all active tasks
-	async findAll() {
+	async findAll(userId: string) {
 		const tasks = await this.prisma.task.findMany({
 			where: {
 				isActive: true,
+				userId: userId,
 			},
 		});
 		return tasks.map((task) => new ResponseTaskDto(task));
 	}
 
 	// Retrieves one task by its ID
-	async findById(id: string): Promise<ResponseTaskDto> {
+	async findById(id: string, userId: string): Promise<ResponseTaskDto> {
 		const taskEntity = await this.prisma.task.findUnique({
 			where: { id },
 		});
 
-		if (!taskEntity) {
+		if (!taskEntity || !taskEntity.isActive) {
 			throw new NotFoundException(`Task with ID "${id}" not found`);
 		}
+
+		if (taskEntity.userId !== userId) {
+			throw new NotFoundException(
+				`Task with ID "${id}" not found for this user.`,
+			);
+		}
+
 		return new ResponseTaskDto(taskEntity);
 	}
 
 	// Creates a new task
-	async createTask(data: CreateTaskDto): Promise<ResponseTaskDto> {
+	async createTask(
+		data: CreateTaskDto,
+		userId: string,
+	): Promise<ResponseTaskDto> {
 		const prismaCreateData = {
 			...data,
+			userId: userId,
 		};
 		const createdTask = await this.prisma.task.create({
 			data: prismaCreateData,
@@ -45,6 +57,7 @@ export class TaskService {
 	async updateTask(
 		taskId: string,
 		updateTaskDto: UpdateTaskDto,
+		userId: string,
 	): Promise<ResponseTaskDto> {
 		const taskEntity = await this.prisma.task.findUnique({
 			where: { id: taskId },
@@ -52,6 +65,12 @@ export class TaskService {
 
 		if (!taskEntity) {
 			throw new NotFoundException(`Task with ID "${taskId}" not found`);
+		}
+
+		if (taskEntity.userId !== userId) {
+			throw new NotFoundException(
+				`Task with ID "${taskId}" not found for this user.`,
+			);
 		}
 
 		const updatedTask = await this.prisma.task.update({
